97e89162b4bbaf43694e0b27a2420d45dcf93a35
Chris F <CF-public@gmx.net>
2022-11-01 01:27:14 +0100

    CaravanDistributeItems removed

--- Source/CustomPatches/HarmonyPatches.cs
+++ Source/CustomPatches/HarmonyPatches.cs
@@ -15,37 +15,7 @@ namespace CustomPatches
 	{
 		static HarmonyPatches()
 		{
-			Harmony harmony = new Harmony("syrus.custompatches");
-
-			harmony.Patch(
-				typeof(CaravanEnterMapUtility).GetMethod("Enter", 
-				new Type[] { typeof(Caravan), typeof(Map), typeof(Func<Pawn, IntVec3>), typeof(CaravanDropInventoryMode), typeof(bool) }),
-				prefix: new HarmonyMethod(typeof(HarmonyPatches), nameof(HarmonyPatches.CaravanEnterMapUtility_Enter_Prefix)));
-			harmony.Patch(
-				typeof(CaravanEnterMapUtility).GetMethod("Enter", 
-				new Type[] { typeof(Caravan), typeof(Map), typeof(Func<Pawn, IntVec3>), typeof(CaravanDropInventoryMode), typeof(bool) }),
-                postfix: new HarmonyMethod(typeof(HarmonyPatches), nameof(HarmonyPatches.CaravanEnterMapUtility_Enter_Postfix)));
-        }
-
-
-        public static void CaravanEnterMapUtility_Enter_Prefix(Caravan caravan, out List<Pawn> __state)
-        {
-            __state = new List<Pawn>();
-
-            if (caravan != null)
-            {
-                // save a list of all pawns that were in the caravan
-                foreach (var pawn in caravan.pawns)
-                    __state.Add(pawn);
-            }
-        }
-        public static void CaravanEnterMapUtility_Enter_Postfix(List<Pawn> __state)
-        {
-            if (!(__state?.Count > 0))
-                return;
-
-            // check for missing sidearms and transfer ones picked up by other pawns back to the correct inventory
-            CaravanDistributeItems.DistributeThingsForPoliciesAndCarry(__state);
+			var harmony = new Harmony("syrus.custompatches");
         }
     }
 }
